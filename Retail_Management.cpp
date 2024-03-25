6d4e23de9e6a01410d1883a0ce49264a5bec161b
Soham <soham.mahapatra2005@gmail.com>
2024-03-25 13:09:25 +0530

    -

--- Retail_Management.cpp
+++ Retail_Management.cpp
@@ -1,13 +1,70 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class shop
+class person
 {
-private:
-    int pcode;
-    
+protected:
+    string name;
+    int phoneNo;
 };
+
+class shop : public person
+{
+protected:
+    int pCode;
+    float price;
+    string pName;
+
+public:
+    void menu();
+    void admin();
+    void buyer();
+    void add();
+    void edit();
+    void remove();
+    void diplayList();
+    void receipt();
+};
+
+void shop ::menu()
+{
+    int choice;
+
+    cout << "\t\t\t\t_____________________________________________________\n";
+    cout << "\t\t\t\t|                                                    |\n";
+    cout << "\t\t\t\t|             Retail Management Menu                 |\n";
+    cout << "\t\t\t\t|                                                    |\n";
+    cout << "\t\t\t\t|____________________________________________________|\n";
+    cout << "\t\t\t\t|                                                    |\n";
+    cout << "\t\t\t\t|     1)Admin                                        |\n";
+    cout << "\t\t\t\t|                                                    |\n";
+    cout << "\t\t\t\t|     2)Buyer                                        |\n";
+    cout << "\t\t\t\t|                                                    |\n";
+    cout << "\t\t\t\t|     3)Exit                                         |\n";
+    cout << "\t\t\t\t|____________________________________________________|\n";
+
+    cout << "\t\t\t\t Please select option(1-3): ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        admin();
+        break;
+
+    case 2:
+        buyer();
+        break;
+    
+    case 3:
+        break;
+    
+    default:
+        break;
+    }
+}
 
 int main()
 {
