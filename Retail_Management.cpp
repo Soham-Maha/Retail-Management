cfb67f141c8a585ee4c0b68ebaa36cea54d40e55
Soham <soham.mahapatra2005@gmail.com>
2024-04-09 11:20:37 +0530

    -

--- Retail_Management.cpp
+++ Retail_Management.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <string>
-#include<fstream>
+#include <fstream>
 #include <chrono>
 #include <ctime>
 
@@ -186,6 +186,12 @@ void shop ::add()
     cin >> price;
     cout << "\n\n\t Discount on product";
     cin >> dis;
+
+    data.open("database.txt", ios::in);
+
+    if(!data){
+        data.open("database.txt", ios::app||ios::out);
+    }
 }
 
 void shop::edit()
@@ -243,45 +249,50 @@ void shop::edit()
     }
 }
 
-void shop ::receipt() {
-  int arrPcode[100];
-  int arrPQuantity[100];
-  int counter = 0;
-  float amount = 0;
-  int bProducts;
-  cout << "kindly enter the total no of different products";
-  cin >> bProducts;
+void shop ::receipt()
+{
+    int arrPcode[100];
+    int arrPQuantity[100];
+    int counter = 0;
+    float amount = 0;
+    int bProducts;
+    cout << "kindly enter the total no of different products";
+    cin >> bProducts;
 
-  for (int i = 0; i < bProducts; i++) {
-    cout << "enter product code";
-    cin >> arrPcode[i];
-    cout << "enter product quantity";
-    cin >> arrPQuantity[i];
-  }
-  for (int i = 0; i < bProducts; i++) {
-    if (arrPcode[i] == pCode[i]) {
-      amount = amount + arrPQuantity[i] * price;
+    for (int i = 0; i < bProducts; i++)
+    {
+        cout << "enter product code";
+        cin >> arrPcode[i];
+        cout << "enter product quantity";
+        cin >> arrPQuantity[i];
     }
-  }
-  cout << "\n\n\t\t\t\t  RECEIPT";
-  cout << "=================================================";
-  cout << "\n\n\t\t\t\t\t\t\t\t";
-  cout << "\n\n\n";
-  auto currentTime =
-      std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-  std::cout << "Time and Date: " << std::ctime(&currentTime);
-  cout << "\n\n\n";
+    for (int i = 0; i < bProducts; i++)
+    {
+        if (arrPcode[i] == pCode[i])
+        {
+            amount = amount + arrPQuantity[i] * price;
+        }
+    }
+    cout << "\n\n\t\t\t\t  RECEIPT";
+    cout << "=================================================";
+    cout << "\n\n\t\t\t\t\t\t\t\t";
+    cout << "\n\n\n";
+    auto currentTime =
+        std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::cout << "Time and Date: " << std::ctime(&currentTime);
+    cout << "\n\n\n";
 
-  cout << "\t\t  SNO.  \t\t  PRODUCT  \t\t  QUANTITY                \t\t  "
-          "PRICE\n\n";
+    cout << "\t\t  SNO.  \t\t  PRODUCT  \t\t  QUANTITY                \t\t  "
+            "PRICE\n\n";
 
-  for (int i = 0; i <= bProducts; i++) {
-    cout << " \t\t " << i + 1 << "  \t\t  " << pName << " \t\t "
-         << arrPQuantity[i] << " \t\t " << arrPQuantity[i] * price << "\n";
-  }
-  cout << "\n\n";
-  cout << "\t\t"
-       << "Your total amount is:  " << amount;
+    for (int i = 0; i <= bProducts; i++)
+    {
+        cout << " \t\t " << i + 1 << "  \t\t  " << pName << " \t\t "
+             << arrPQuantity[i] << " \t\t " << arrPQuantity[i] * price << "\n";
+    }
+    cout << "\n\n";
+    cout << "\t\t"
+         << "Your total amount is:  " << amount;
 }
 
 int main()
